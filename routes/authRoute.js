8c854b50c9e4219d99b177caa17f434f70ec7f6f
Priyansh Verma <verma.priyansh444@gmail.com>
2023-11-14 15:38:03 +0530

    added authMiddleware file

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,22 @@
-import express from "express"
-import { registerController } from "../controllers/authController.js";
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
 //router object
-const router = express.Router()
+const router = express.Router();
 
 //routing
-//REISTER || METHOD POST
-router.post("/register",registerController)
+//REGISTER || METHOD POST
+router.post("/register", registerController);
 
-export default router
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+export default router;
