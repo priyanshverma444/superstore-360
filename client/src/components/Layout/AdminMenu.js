a76367ca486bb955d224e99f9f7a4faad1cf85d8
Priyansh Verma <verma.priyansh444@gmail.com>
2024-01-16 02:17:36 +0530

    CRUD operations done

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 const AdminMenu = () => {
   return (
-    <> 
-<div class='text-center'>
-<div class="list-group">
-  <h4>Admin Panel</h4>
-  <NavLink to="/dashboard/admin/create-category" class="list-group-item list-group-item-action">Create Category</NavLink>
-  <NavLink to="/dashboard/admin/create-product" class="list-group-item list-group-item-action">Create Product</NavLink>
-  <NavLink to="/dashboard/admin/users" class="list-group-item list-group-item-action">Users</NavLink>
-</div>
-</div>
+    <>
+      <div className="text-center">
+        <div className="list-group">
+          <h4>Admin Panel</h4>
+          <NavLink
+            to="/dashboard/admin/create-category"
+            className="list-group-item list-group-item-action"
+          >
+            Create Category
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/create-product"
+            className="list-group-item list-group-item-action"
+          >
+            Create Product
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/products"
+            className="list-group-item list-group-item-action"
+          >
+            Products
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/users"
+            className="list-group-item list-group-item-action"
+          >
+            Users
+          </NavLink>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AdminMenu
+export default AdminMenu;
