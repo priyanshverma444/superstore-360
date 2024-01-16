1fd51e16b58b138d77f77adf4449249d5d084771
Priyansh Verma <verma.priyansh444@gmail.com>
2024-01-16 20:51:45 +0530

    added cart

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,8 +4,12 @@ import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
 import SearchInput from "../Form/SearchInput";
 import useCategory from "../../hooks/useCategory";
+import { useCart } from "../../context/cart";
+import { Badge } from "antd";
+
 const Header = () => {
   const [auth, setAuth] = useAuth();
+  const [cart] = useCart();
   const categories = useCategory();
   const handleLogout = () => {
     setAuth({
@@ -38,7 +42,7 @@ const Header = () => {
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <SearchInput />
               <li className="nav-item">
-                <NavLink to="/" className="nav-link">
+                <NavLink to="/" className="nav-link ">
                   Home
                 </NavLink>
               </li>
@@ -119,9 +123,11 @@ const Header = () => {
                 </>
               )}
               <li className="nav-item">
-                <NavLink to="/cart" className="nav-link">
-                  Cart (0)
-                </NavLink>
+                <Badge count={cart?.length} showZero>
+                  <NavLink to="/cart" className="nav-link">
+                    Cart
+                  </NavLink>
+                </Badge>
               </li>
             </ul>
           </div>
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,6 +4,7 @@ import {
   loginController,
   testController,
   forgotPasswordController,
+  updateProfileController,
 } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
@@ -17,19 +18,22 @@ router.post("/register", registerController);
 //LOGIN || POST
 router.post("/login", loginController);
 
-//FORGOT PASSWORD || POST
-router.post('/forgot-password', forgotPasswordController);
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
 
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
 
-//protected user route auth
-router.get("/user-auth", requireSignIn, (req,res) => {
-  res.status(200).send({ok:true});
+//protected User route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+//protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
 });
 
-//protected admin route auth
-router.get("/admin-auth", requireSignIn,isAdmin, (req,res) => {
-  res.status(200).send({ok:true});
-});
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
 export default router;
