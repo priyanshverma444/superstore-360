2544379c8f152c4051554a6e4d55a83fd11c1d61
Priyansh Verma <verma.priyansh444@gmail.com>
2024-03-02 00:46:55 +0530

    added preloader animation

--- client/src/App.js
+++ client/src/App.js
@@ -24,38 +24,83 @@ import Categories from "./pages/Categories";
 import CategoryProduct from "./pages/CategoryProduct";
 import CartPage from "./pages/CartPage";
 import AdminOrders from "./pages/Admin/AdminOrders";
+import React, { useState, useEffect } from "react";
+import { quantum } from "ldrs";
+import axios from "axios";
+
 function App() {
+  const [checked] = useState([]);
+  const [radio] = useState([]);
+  const [setProducts] = useState([]);
+  const [page] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const getAllProducts = async () => {
+    try {
+      setLoading(true);
+      quantum.register();
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/product-list/${page}`
+      );
+      setLoading(false);
+      setProducts(data.products);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (!checked.length || !radio.length) getAllProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [checked.length, radio.length]);
+  // useEffect(() => {
+  //   quantum.register();
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 40000);
+  // }, []);
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/product/:slug" element={<ProductDetails />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/category/:slug" element={<CategoryProduct />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="user" element={<Dashboard />} />
-          <Route path="user/orders" element={<Orders />} />
-          <Route path="user/profile" element={<Profile />} />
-        </Route>
-        <Route path="/dashboard" element={<AdminRoute />}>
-          <Route path="admin" element={<AdminDashboard />} />
-          <Route path="admin/create-category" element={<CreateCategory />} />
-          <Route path="admin/create-product" element={<CreateProduct />} />
-          <Route path="admin/product/:slug" element={<UpdateProduct />} />
-          <Route path="admin/products" element={<Products />} />
-          <Route path="admin/users" element={<Users />} />
-          <Route path="admin/orders" element={<AdminOrders />} />
-        </Route>
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPasssword />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/policy" element={<Policy />} />
-        <Route path="*" element={<Pagenotfound />} />
-      </Routes>
+      {loading ? (
+        <div
+          className="d-flex flex-column justify-content-center align-items-center bg-dark text-white"
+          style={{ height: "100vh" }}
+          data-bs-theme="dark"
+        >
+          <l-quantum size="125" speed="1.50" color="red"></l-quantum>
+          <h1 className="mt-4">Loading, Please wait...</h1>
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:slug" element={<ProductDetails />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/category/:slug" element={<CategoryProduct />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/dashboard" element={<PrivateRoute />}>
+            <Route path="user" element={<Dashboard />} />
+            <Route path="user/orders" element={<Orders />} />
+            <Route path="user/profile" element={<Profile />} />
+          </Route>
+          <Route path="/dashboard" element={<AdminRoute />}>
+            <Route path="admin" element={<AdminDashboard />} />
+            <Route path="admin/create-category" element={<CreateCategory />} />
+            <Route path="admin/create-product" element={<CreateProduct />} />
+            <Route path="admin/product/:slug" element={<UpdateProduct />} />
+            <Route path="admin/products" element={<Products />} />
+            <Route path="admin/users" element={<Users />} />
+            <Route path="admin/orders" element={<AdminOrders />} />
+          </Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPasssword />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/policy" element={<Policy />} />
+          <Route path="*" element={<Pagenotfound />} />
+        </Routes>
+      )}
     </>
   );
 }
